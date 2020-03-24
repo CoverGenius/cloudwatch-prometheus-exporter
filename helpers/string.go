c2678ca5bd880fb0218dc790977e11f85bdc974e
awright <anderw.w@covergenius.com>
2020-03-24 16:46:54 +1100

    Bump changelog and add helper for easier config

--- helpers/string.go
+++ helpers/string.go
@@ -9,3 +9,11 @@ func FormatString(format string, args ...string) string {
 	out := boilerplate.Replace(format)
 	return out
 }
+
+func AWSStrings(strings ...string) []*string {
+	aws_strings := make([]*string, len(strings))
+	for i, _ := range aws_strings {
+		aws_strings[i] = &strings[i]
+	}
+	return aws_strings
+}
